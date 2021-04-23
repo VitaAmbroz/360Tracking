082fd8ce789a77899101db4c2974a70a71e287df
VitaAmbroz <ambroz.vita@gmail.com>
2021-04-23 18:59:02 +0200

    Added opencv tracking with rectilinear improvement

---
+++ code/nfov/__init__.py
@@ -0,0 +1,1 @@
+from .nfov import NFOV
--- code/opencv_tracking/opencv_tracking.py
+++ code/opencv_tracking/opencv_tracking.py
@@ -18,10 +18,10 @@ currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
+# from boundingbox.boundingbox import BoundingBox
 from code.boundingbox import BoundingBox
 from code.boundingbox import Parser
-# from boundingbox.boundingbox import BoundingBox
-
+from code.nfov import NFOV
 
 class OpenCVTracking:
     def __init__(self, trackerName: str, videoPath: str, groundtruthPath: str, saveResultPath: str):
@@ -32,7 +32,7 @@ class OpenCVTracking:
         if saveResultPath:
             self.save_result_path = saveResultPath
         else:    
-            self.save_result_path = "tmp-results-" + self.tracker_name + ".txt"
+            self.save_result_path = "tmp-result-" + self.tracker_name + ".txt"
 
         self.video = None
         self.video_width = None
@@ -50,7 +50,7 @@ class OpenCVTracking:
         # constants for sizes and positions of opencv circles, rectangles and texts
         self.RECTANGLE_BORDER_PX = 2
         self.FONT_SCALE = 0.75
-        self.FONT_WEIGHT = 2
+        self.FONT_WEIGHT = 1
         self.TEXT_ROW1_POS = (30,30)
         self.TEXT_ROW2_POS = (30,60)
         self.TEXT_ROW3_POS = (30,90)
@@ -498,8 +498,283 @@ class OpenCVTracking:
         cv2.destroyAllWindows()
 
 
+    # method for start opencv tracking with improvement of mapping equirectangular to rectilinear projection
     def startTrackingRectilinear(self):
-        print("start tracking rectilinear")
+        # prints just basic guide and info
+        print("--------------------------------------------------------------------")
+        print("OpenCV tracking process with rectilinear improvement has started...")
+        print("Tracker  : " + self.tracker_name)
+        print("Frame #1 : " + str(self.bbox))
+        print("Press 'Esc' or 'Q' key to exit")
+        print("--------------------------------------------------------------------")
+
+        ##########################################################################
+        ################## Normal field of view initialization ###################
+        ##########################################################################
+        # init instance for normal field of view according to rectilinear framework
+        nfov_width = int(self.video_width / 2)
+        nfov_height = int(self.video_height / 2)
+
+        # lets define max rectilinear window size - 720p
+        if nfov_width > 1440 or nfov_height > 720:
+            whRatio = nfov_width / nfov_height
+            nfov_height = 720
+            nfov_width = round(whRatio * 720)
+
+        # TODO resize for 4K, maybe also for fullHD 
+        nfov = NFOV(nfov_height, nfov_width)
+
+        # center point of selected bounding box
+        center_equi_x = int(self.bbox[0] + self.bbox[2]/2)
+        center_equi_y = int(self.bbox[1] + self.bbox[3]/2)
+        center_equi_x_normalized = center_equi_x / self.video_width
+        center_equi_y_normalized = center_equi_y / self.video_height
+        
+        # bounding box points left_top and bottom_right
+        x1_normalized = self.bbox[0] / self.video_width
+        y1_normalized = self.bbox[1] / self.video_height
+        x2_normalized = (self.bbox[0] + self.bbox[2]) / self.video_width
+        y2_normalized = (self.bbox[1] + self.bbox[3]) / self.video_height
+
+        # camera center point (valid range [0,1])
+        center_point = np.array([center_equi_x_normalized, center_equi_y_normalized])
+        # bounding box points left_top and bottom_right
+        nfov.point1_equi = np.array([x1_normalized, y1_normalized])
+        nfov.point2_equi = np.array([x2_normalized, y2_normalized])
+        # remap to normal field of view
+        frameRectilinear = nfov.toNFOV(self.frame, center_point, computeRectPoints=True)
+
+        # get coordinates of points in rectilinear projection
+        x1_rect = nfov.point1_rect[0]
+        y1_rect = nfov.point1_rect[1]
+        width_rect = nfov.point2_rect[0] - nfov.point1_rect[0]
+        height_rect = nfov.point2_rect[1] - nfov.point1_rect[1]
+
+
+        ##########################################################################
+        ################## Tracking process initialization #######################
+        ##########################################################################
+        # use bounding box representation also in rectilinear
+        bbox_rect = (x1_rect, y1_rect, width_rect, height_rect)
+
+        # top left in equirectangular
+        p1_equi = (int(self.bbox[0]), int(self.bbox[1]))
+        # bottom right in equirectangular
+        p2_equi = (int(self.bbox[0] + self.bbox[2]), int(self.bbox[1] + self.bbox[3]))
+
+        # top left in rectilinear
+        p1_rect = (bbox_rect[0], bbox_rect[1])
+        # bottom right in rectilinear
+        p2_rect = (bbox_rect[0] + bbox_rect[2], bbox_rect[1] + bbox_rect[3])
+
+        # display first frame
+        cv2.rectangle(self.frame, p1_equi, p2_equi, (0, 255, 0), 2, 1)
+        cv2.imshow(self.WINDOW_NAME, self.frame)
+
+        cv2.namedWindow(self.WINDOW_NAME_RECTILINEAR)
+        cv2.rectangle(frameRectilinear, p1_rect, p2_rect, (255, 255, 0), 2, 1)
+        cv2.imshow(self.WINDOW_NAME_RECTILINEAR, frameRectilinear)
+
+        # initialize tracker with first frame and bounding box
+        ok = self.tracker.init(frameRectilinear, bbox_rect)
+
+        # max fps
+        videoFPS = 30
+        # videoFPS = cap.get(cv2.CAP_PROP_FPS) 
+        # calculate the interval between frame
+        interval = int(1000/videoFPS) 
+
+        # counter for frames
+        self.current_frame = 1
+
+        cv2.waitKey(0)
+
+        # empiric constants for shifting/scaling in rectilinear projection - setup by experiments
+        SHIFT_SLOW_X_START = 0.45 * nfov_width
+        SHIFT_SLOW_Y_START = 0.45 * nfov_height
+        SHIFT_FAST_X_START = 0.35 * nfov_width
+        SHIFT_FAST_Y_START = 0.35 * nfov_height
+
+        SHIFT_FAST_X = int(self.video_width / 100)
+        SHIFT_FAST_Y = int(self.video_height / 100)
+
+        SHIFT_SLOW_X = int(self.video_width / 200)
+        SHIFT_SLOW_Y = int(self.video_height / 200)
+
+        SCALEDOWN_FOV_SLOW_START_X = 0.66 * nfov_width
+        SCALEDOWN_FOV_SLOW_START_Y = 0.66 * nfov_height
+
+        SCALEDOWN_FOV_FAST_START_X = 0.8 * nfov_width
+        SCALEDOWN_FOV_FAST_START_Y = 0.8 * nfov_height
+        
+        SCALE_FOV_STEP_SLOW = 0.01
+        SCALE_FOV_STEP_FAST = 0.02
+
+        while True:
+            # Read a new frame
+            ok, self.frame = self.video.read()
+            if not ok:
+                break
+            # increment counter
+            self.current_frame += 1
+
+            # Start timer
+            timer = cv2.getTickCount()
+            
+            # update center point
+            if p1_rect and p2_rect and bbox_rect:
+                # center of bounding box in rectilinear projection
+                center_rect = [bbox_rect[0] + bbox_rect[2]/2, bbox_rect[1] + bbox_rect[3]/2]
+
+                # SHIFTS X
+                # FAST
+                if center_rect[0] < SHIFT_FAST_X_START:
+                    center_equi_x -= SHIFT_FAST_X
+                    if center_equi_x < 0:
+                        center_equi_x = self.video_width + center_equi_x
+                    else:
+                        center_equi_x = center_equi_x % self.video_width
+                elif center_rect[0] > nfov_width - SHIFT_FAST_X_START:
+                    center_equi_x += SHIFT_FAST_X
+                    center_equi_x = center_equi_x % self.video_width
+                # SLOW
+                elif center_rect[0] < SHIFT_SLOW_X_START:
+                    center_equi_x -= SHIFT_SLOW_X
+                    if center_equi_x < 0:
+                        center_equi_x = self.video_width + center_equi_x
+                    else:
+                        center_equi_x = center_equi_x % self.video_width
+                elif center_rect[0] > nfov_width - SHIFT_SLOW_X_START:
+                    center_equi_x += SHIFT_SLOW_X
+                    center_equi_x = center_equi_x % self.video_width
+                
+
+                # SHIFTS Y
+                # FAST
+                if center_rect[1] < SHIFT_FAST_Y_START:
+                    center_equi_y -= SHIFT_FAST_Y
+                    if center_equi_y < 0:
+                        center_equi_y = self.video_height + center_equi_y
+                    else:
+                        center_equi_y = center_equi_y % self.video_height
+                elif center_rect[1] > nfov_height - SHIFT_FAST_Y_START:
+                    center_equi_y += SHIFT_FAST_Y
+                    center_equi_y = center_equi_y % self.video_height
+                # SLOW Y
+                elif center_rect[1] < SHIFT_SLOW_Y_START:
+                    center_equi_y -= SHIFT_SLOW_Y
+                    if center_equi_y < 0:
+                        center_equi_y = self.video_height + center_equi_y
+                    else:
+                        center_equi_y = center_equi_y % self.video_height
+                elif center_rect[1] > nfov_height - SHIFT_SLOW_Y_START:
+                    center_equi_y += SHIFT_SLOW_Y
+                    center_equi_y = center_equi_y % self.video_height
+
+
+                # rescale FOV - enable only zoom down (further from object)
+                # object is close to camera/big -> increase field of view
+                # FAST
+                if bbox_rect[2] > SCALEDOWN_FOV_FAST_START_X or bbox_rect[3] > SCALEDOWN_FOV_FAST_START_Y:
+                    if nfov.FOV[0] < 0.9:
+                        nfov.FOV = [nfov.FOV[0] + SCALE_FOV_STEP_FAST, nfov.FOV[1] + SCALE_FOV_STEP_FAST]
+                # SLOW
+                elif bbox_rect[2] > SCALEDOWN_FOV_SLOW_START_X or bbox_rect[3] > SCALEDOWN_FOV_SLOW_START_Y:
+                    if nfov.FOV[0] < 0.9:
+                        nfov.FOV = [nfov.FOV[0] + SCALE_FOV_STEP_SLOW, nfov.FOV[1] + SCALE_FOV_STEP_SLOW]
+                    
+                # disabled + zoom
+                # elif bbox_rect[2] < SCALEDOWN_FOV_FAST_START_X and bbox_rect[3] < SCALEDOWN_FOV_FAST_START_Y:
+                #     # object is small -> decrease field of view
+                #     nfov.FOV = [nfov.FOV[0] - SCALE_FOV_STEP_FAST, nfov.FOV[1] - SCALE_FOV_STEP_FAST]
+                # elif bbox_rect[2] < SCALEDOWN_FOV_SLOW_START_X and bbox_rect[3] < SCALEDOWN_FOV_SLOW_START_Y:
+                #     # object is small -> decrease field of view
+                #     nfov.FOV = [nfov.FOV[0] - SCALE_FOV_STEP_SLOW, nfov.FOV[1] - SCALE_FOV_STEP_SLOW]
+
+            # normalize center point in [0,1]
+            center_equi_x_normalized = center_equi_x / self.video_width
+            center_equi_y_normalized = center_equi_y / self.video_height
+            # camera center point (valid range [0,1])
+            center_point = np.array([center_equi_x_normalized, center_equi_y_normalized])  
+
+            # new frame to rectilinear/normal field of view
+            frameRectilinear = nfov.toNFOV(self.frame, center_point)
+
+            # Update tracker
+            ok, bbox_rect = self.tracker.update(frameRectilinear)
+
+            # calculate Frames per second after tracking (FPS)
+            fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+
+            # handle tracking result
+            if ok:
+                # rectilinear
+                p1_rect = (round(bbox_rect[0]), round(bbox_rect[1]))
+                p2_rect = (round(bbox_rect[0] + bbox_rect[2]), round(bbox_rect[1] + bbox_rect[3]))
+                # store points to nfov instance
+                nfov.point1_rect = [p1_rect[0], p1_rect[1]]
+                nfov.point2_rect = [p2_rect[0], p2_rect[1]]
+                # draw bounding box to rectilinear frame
+                cv2.rectangle(frameRectilinear, p1_rect, p2_rect, (255, 255, 0), self.RECTANGLE_BORDER_PX, 1)
+
+                # equirectangular
+                # compute corresponding points of rectilinear bounding box in equirectangular projection 
+                nfov.computeEquirectangularBbox(bbox_width=round(bbox_rect[2]), bbox_height=round(bbox_rect[3]))
+                # bbox points top left and right bottom in equirectangular projection
+                p1_equi = (round(nfov.point1_equi[0] * self.video_width), round(nfov.point1_equi[1] * self.video_height))
+                p2_equi = (round(nfov.point2_equi[0] * self.video_width), round(nfov.point2_equi[1] * self.video_height))
+
+                # create custom equirectangular bounding box instance
+                bb = BoundingBox(p1_equi, p2_equi, self.video_width)
+                bb.is_annotated = True
+                self.result_bounding_boxes.append(bb)
+
+                # draw bounding box to original equirectangular frame
+                self._drawBoundingBox(self.video_width, p1_equi, p2_equi, bb, (0, 255, 0), self.RECTANGLE_BORDER_PX)
+            else:
+                # tracking failure
+                cv2.putText(self.frame, "Tracking failure detected", self.TEXT_ROW4_POS, cv2.FONT_HERSHEY_SIMPLEX, self.FONT_SCALE, (0, 0, 255), self.FONT_WEIGHT)
+                
+                # reinit points
+                p1_rect = None
+                p2_rect = None
+                p1_equi = None
+                p2_equi = None
+
+                # create custom empty equirectangular bounding box instance
+                bb = BoundingBox(None, None, self.video_width)
+                bb.is_annotated = False
+                self.result_bounding_boxes.append(bb)
+            
+            # Display tracker type on frame
+            cv2.putText(self.frame, self.tracker_name + " Tracker", self.TEXT_ROW1_POS, cv2.FONT_HERSHEY_SIMPLEX, self.FONT_SCALE, (0, 255, 0), self.FONT_WEIGHT)
+            # Display FPS on frame
+            cv2.putText(self.frame, "Video   FPS : " + str(videoFPS), self.TEXT_ROW2_POS, cv2.FONT_HERSHEY_SIMPLEX, self.FONT_SCALE, (0, 255, 0), self.FONT_WEIGHT)
+            cv2.putText(self.frame, "Tracker FPS : " + str(int(fps)), self.TEXT_ROW3_POS, cv2.FONT_HERSHEY_SIMPLEX, self.FONT_SCALE, (0, 255, 0), self.FONT_WEIGHT)
+            
+            # Display result
+            cv2.imshow(self.WINDOW_NAME, self.frame)
+            cv2.imshow(self.WINDOW_NAME_RECTILINEAR, frameRectilinear)
+
+
+            # waitKey time computing
+            # time in ms
+            time = int(1000 * (cv2.getTickCount() - timer) / cv2.getTickFrequency())
+
+            waitMiliseconds = 1
+            if (time >= interval):
+                waitMiliseconds = 1
+            else:
+                waitMiliseconds = interval - time
+            
+            k = cv2.waitKey(waitMiliseconds) & 0xff
+            # Exit if 'Esc' or 'q' key is pressed
+            if k == 27 or k == ord("q"): 
+                break
+        
+        # release, destroy
+        self.video.release()
+        cv2.destroyAllWindows()
 
 
     def startTrackingCentering(self):
